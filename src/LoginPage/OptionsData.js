c67092c06bd58ac510c29dc8df98c51f5e9c6134
Tanaka878 <musungaretanaka@gmail.com>
2024-11-04 09:10:54 +0200

    commit

--- src/LoginPage/OptionsData.js
+++ src/LoginPage/OptionsData.js
@@ -1,56 +1,52 @@
 // eslint-disable-next-line import/no-anonymous-default-export
-export default[
+export default [
     {
-        "id":1,
-        "itemName":"balanceRequest",
-        "val":true,
-        'url':'balance.png',
-        'textValue':'Bank Statements'
+        "id": 1,
+        "itemName": "balanceRequest",
+        "val": true,
+        "url": "balance.png",
+        "textValue": "Bank Statements"
     },
-
     {
-        "id":2,
-        "itemName":"feesImage",
-        "val":false,
-        "url":'PayFees.png',
-        'textValue':'PAY FESS'
+        "id": 2,
+        "itemName": "feesImage",
+        "val": false,
+        "url": "PayFees.png",
+        "textValue": "PAY FEES"
     },
-
     {
-        "id":3,
-        "itemName":"phone",
-        "url":"phone.png",
-        'textValue':'BUY AIRTIME'
+        "id": 3,
+        "itemName": "phone",
+        "val": true, // or false, depending on your requirement
+        "url": "phone.png",
+        "textValue": "BUY AIRTIME"
     },
-
     {
-        "id":4,
-        "itemName":"Cash",
-        "url":"Money.png",
-        'textValue':'ZIP IT TRANSFER'
+        "id": 4,
+        "itemName": "Cash",
+        "val": true, // or false, depending on your requirement
+        "url": "Money.png",
+        "textValue": "ZIP IT TRANSFER"
+    },
+    {
+        "id": 5,
+        "itemName": "Bills",
+        "val": true, // or false, depending on your requirement
+        "url": "paying.webp",
+        "textValue": "Bill Payments"
+    },
+    {
+        "id": 6,
+        "itemName": "PayPal",
+        "val": true, // or false, depending on your requirement
+        "url": "Paypal.png",
+        "textValue": "Top Up"
+    },
+    {
+        "id": 7,
+        "itemName": "Change Password",
+        "val": true, // or false, depending on your requirement
+        "url": "pay.png",
+        "textValue": "Change Password"
     }
-
-    ,
-    {
-        "id":5,
-        "itemName":"Bills",
-        "url":"paying.webp",
-        "textValue":"Bill Payments"
-    }
-    ,
-    {
-        "id":6,
-        "itemName":"PayPal",
-        "url":"Paypal.png",
-        "textValue":"Top Up"
-    },
-
-    {
-        "id":7,
-        "itemName":"Change Password",
-        "url":"pay.png",
-        "textValue":"Change Password"
-    },
-
-    
-]
+];
