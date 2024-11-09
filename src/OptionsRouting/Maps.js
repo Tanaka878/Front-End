ad5cc9c2fff223d49e7cc0fe1d3b37b1c6302d4f
Tanaka878 <musungaretanaka@gmail.com>
2024-11-09 18:47:38 +0200

    commit

--- src/OptionsRouting/Maps.js
+++ src/OptionsRouting/Maps.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import AddressSearch from './AddressSearch';
+import AddressSearch from './AdressSearch';
 import ATMMap from './ATMMap';
 
 const Maps = () => {
