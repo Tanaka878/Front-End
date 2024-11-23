530960805a7c064cc3a233330033710615682b52
Tanaka878 <musungaretanaka@gmail.com>
2024-11-23 12:56:05 +0200

    commit

--- src/OptionsRouting/AdressSearch.js
+++ src/OptionsRouting/AdressSearch.js
@@ -1,31 +1,14 @@
 import React, { useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-const AddressSearch = ({ onATMFound }) => {
+const AddressSearch = () => {
   const [address, setAddress] = useState('');
-
-  const navigation = useNavigate()
-
-  const handleSearch = () => {
-    axios
-      .get(`/api/atm/geocode?address=${address}`)
-      .then((response) => {
-        const location = JSON.parse(response.data).results[0].geometry.location;
-        console.log('Geocode Location:', location);  // Log the location for debugging
-        onATMFound(location);
-      })
-      .catch((error) => console.error(error));
-      
-     navigation('optionPage');
-
-
-  };
 
   return (
     <div>
-      
-      <button onClick={handleSearch}>Exit Navigation</button>
+      <Link to="/maps/optionPage">
+        <button>Exit Navigation</button>
+      </Link>
     </div>
   );
 };
