42e0756beccba8518cd56d4e6f2b54fbcd0c0fcb
Tanaka878 <musungaretanaka@gmail.com>
2025-07-24 18:31:44 +0200

    Update API base URL

--- src/OptionsRouting/api.js
+++ src/OptionsRouting/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://distinguished-happiness-production.up.railway.app'
+  baseURL: 'https://accutebanking.thetalisman.co.zw'
 });
 
 export default api;
