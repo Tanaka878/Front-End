a3c1cd31288969ad3f2a0754a7d591c0d2d82053
Tanaka878 <musungaretanaka@gmail.com>
2025-07-25 04:10:01 +0200

    making chnages for deployment

--- src/OptionsRouting/api.js
+++ src/OptionsRouting/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8082'
+  baseURL: 'https://accutebanking.thetalisman.co.zw'
 });
 
 export default api;
 
 
-export const baseURL = 'http://localhost:8082';
+export const baseURL = 'https://accutebanking.thetalisman.co.zw';
